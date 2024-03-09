f1f426152cc2ab33d1bf0299923df6b9519a2185
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-03-09 21:14:36 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/widgets/reconhecimento_facial.dart
+++ lib/custom_code/widgets/reconhecimento_facial.dart
@@ -17,68 +17,12 @@ class ReconhecimentoFacial extends StatefulWidget {
   final double? height;
 
   @override
-  _ReconhecimentoFacialState createState() => _ReconhecimentoFacialState();
+  State<ReconhecimentoFacial> createState() => _ReconhecimentoFacialState();
 }
 
 class _ReconhecimentoFacialState extends State<ReconhecimentoFacial> {
-  late final CameraController _cameraController;
-
-  @override
-  void initState() {
-    super.initState();
-    _cameraController = CameraController(
-      // Seleciona a câmera frontal
-      cameras.firstWhere(
-          (camera) => camera.lensDirection == CameraLensDirection.front),
-      ResolutionPreset.medium,
-    );
-    _cameraController.initialize().then((_) {
-      if (!mounted) return;
-      setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    _cameraController.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        // Câmera
-        _cameraController.value.isInitialized
-            ? AspectRatio(
-                aspectRatio: _cameraController.value.aspectRatio,
-                child: CameraPreview(_cameraController),
-              )
-            : Container(),
-      ],
-    );
-  }
-
-  Widget _buildButtons() {
-    return Positioned(
-      bottom: 20.0,
-      left: 20.0,
-      right: 20.0,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          // Botão 1
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('Botão 1'),
-          ),
-          // Botão 2
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('Botão 2'),
-          ),
-        ],
-      ),
-    );
+    return Container();
   }
 }
